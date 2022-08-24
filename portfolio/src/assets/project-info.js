d60f1fb4ded814c100b576ae252a7bbe81556c42
Laura Jazmin Anderson <jazminandersonxo@gmail.com>
2022-08-24 11:56:15 -0700

    pages and installed react pdf

---
+++ portfolio/src/assets/project-info.js
@@ -0,0 +1,37 @@
+export const projectInfo = [
+    {
+        title: 'Team Profile Generator',
+        technologies: ["Node.js", "MySQL", "Express", "JavaScript", "CSS", "Jest"],
+        thumbnail: 'Randm.jpg',
+        repo: 'https://github.com/andersonjaz/Team-Profile-Gen/',
+        url: 'https://andersonjaz.github.io/Team-Profile-Gen/'
+    },
+    {
+        title: 'Password Generator',
+        technologies: [ "HTML", "CSS", "JavaScript"],
+        thumbnail: 'TechBlog.png',
+        repo: 'https://github.com/andersonjaz/Password-Generator',
+        url: 'https://andersonjaz.github.io/Password-Generator/'
+    },
+    {
+        title: 'Beer O Clock',
+        technologies: ["HTML", "JavaScript", "jQuery", "DOM", "API", "LocalStorage", "Moment.js", "Foundation / CSS"],
+        thumbnail: 'TravelItinerary.jpg',
+        repo: 'https://github.com/El-Mariachi760/beer-pages',
+        url: 'https://el-mariachi760.github.io/beer-pages/'
+    },
+    {
+        title: 'Weather Dashboard',
+        technologies: ["HTML", "CSS", "JavaScript", "DOM", "API"],
+        thumbnail: 'WeatherDashboard.jpg',
+        repo: 'https://github.com/AndersonJaz/The-Weather-Dashboard',
+        url: 'https://andersonjaz.github.io/The-Weather-Dashboard/'
+    },
+    {
+        title: 'ARC Auto Repair',
+        technologies: ["Node.js", "Inquirer", "Jest", "HTML", "CSS"],
+        thumbnail: 'TeamProfileGenerator.jpg',
+        repo: 'https://github.com/andersonjaz/ARC-Website',
+        url: 'https://andersonjaz.github.io/ARC-Website/'
+    }
+];
